9682605d48fb2f9df01a1476da3b6467e551165b
Tom Maullin <tommaullin@gmail.com>
2016-09-26 10:32:44 +0100

    tag > type in results code

--- lib/WorkingNIDMResultsExtraction.m
+++ lib/WorkingNIDMResultsExtraction.m
@@ -1,9 +1,9 @@
-aa=spm_jsonread('C:\Users\owner\Documents\Project - NIDASH\NIDMtestpack1.json')
+aa=spm_jsonread('C:\Users\owner\Documents\Project-NIDASH\NIDMtestpack1.json')
 graph = aa.('@graph')
 
 % finding title
 
-temp = searchforTag('nidm_ContrastMap', graph)
+temp = searchforType('nidm_ContrastMap', graph)
 
 for i = 1:length(temp)
     if isfield(temp{i}, 'nidm_contrastName')
@@ -13,12 +13,12 @@ end
 
 % finding contrast vector
 
-contrastWeightMatrix = searchforTag('obo_contrastweightmatrix', graph)
+contrastWeightMatrix = searchforType('obo_contrastweightmatrix', graph)
 contrastVector = contrastWeightMatrix{1}.('prov:value')
 
 % finding set level p & c
 
-excursionSetMap = searchforTag('nidm_ExcursionSetMap', graph)
+excursionSetMap = searchforType('nidm_ExcursionSetMap', graph)
 setP = excursionSetMap{1}.('nidm_pValue').('@value')
 setC = excursionSetMap{1}.('nidm_numberOfSupraThresholdClusters').('@value')
 
--- lib/searchforType.m
+++ lib/searchforType.m
@@ -1,14 +1,14 @@
-function result = searchforTag(tag, graph) 
+function result = searchforType(type, graph) 
  
     result = []
     n = 1
     for k = 1:331
-        if any(ismember(graph{k}.('@type'), tag)) && isa(graph{k}.('@type'), 'cell')
+        if any(ismember(graph{k}.('@type'), type)) && isa(graph{k}.('@type'), 'cell')
             result{n} = graph{k}
             n = n+1
         end
         if isa(graph{k}.('@type'), 'char')
-            if strcmp(graph{k}.('@type'), tag)
+            if strcmp(graph{k}.('@type'), type)
                 result{n} = graph{k}
                 n = n+1
             end
