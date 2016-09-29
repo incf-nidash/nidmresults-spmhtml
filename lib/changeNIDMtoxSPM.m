baf459d9b9f97a064bd9a6629abe174b85c453cd
Tom Maullin <tommaullin@gmail.com>
2016-09-29 14:31:35 +0100

    progress on changeNIDMtoSPM

--- lib/changeNIDMtoxSPM.m
+++ lib/changeNIDMtoxSPM.m
@@ -5,17 +5,53 @@ function NxSPM = changeNIDMtoxSPM(xSPM, json)
     NxSPM = xSPM
     
     %==============================================
-    %Retrieve title
+    %title
+    
     temp = searchforType('nidm_ContrastMap', graph)
-    name = []
     for i = 1:length(temp)
         if isfield(temp{i}, 'nidm_contrastName')
-            name = temp{i}.('nidm_contrastName')
+            titleTemp = temp{i}.('nidm_contrastName')
         end
     end 
-    NxSPM.title = name
     
     %==============================================
-    %Retrieve
+    %STAT
+    
+    temp = searchforType('nidm_StatisticMap', graph)
+    for i = 1:length(temp)
+        if isfield(temp{i}, 'nidm_statisticType')
+            statType = temp{i}.('nidm_statisticType').('@id')
+        end
+    end
+    
+    if strcmp(statType, 'obo:STATO_0000176')
+       STATTemp = 'T'
+    elseif strcmp(statType, 'obo:STATO_0000030')
+       STATTemp = 'X'
+    elseif strcmp(statType, 'obo:STATO_0000378')
+       STATTemp = 'Z'
+    elseif strcmp(statType, 'obo:STATO_0000282')
+       STATTemp = 'F'
+    else
+       STATTemp = 'P'
+    end
+       
+    %===============================================
+    %STATStr
+    
+    temp = searchforType('nidm_StatisticMap', graph)
+    for i = 1:length(temp)
+        if isfield(temp{i}, 'nidm_errorDegreesOfFreedom')
+            errorDegrees = temp{i}.('nidm_errorDegreesOfFreedom').('@value')
+        end
+    end 
+    errorDegrees = num2str(round(str2num(errorDegrees)))
+    STATStrTemp = [STATTemp '_{' errorDegrees '}']
+    
+    %===============================================
+    
+    NxSPM.title = titleTemp
+    NxSPM.STAT = STATTemp
+    NxSPM.STATStr = STATStrTemp
     
 end
