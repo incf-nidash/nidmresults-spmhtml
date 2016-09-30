b2651d212e59fa77c20750e9e67f926752973966
Tom Maullin <tommaullin@gmail.com>
2016-09-30 15:43:05 +0100

    function for display added

---
+++ nidm_results_display.m
@@ -0,0 +1,6 @@
+function nidm_results_display(jsonfilepath)
+    json=spm_jsonread(jsonfilepath)
+    [pathstr,name,ext] = fileparts(jsonfilepath) 
+    json.filepath = pathstr
+    spm_results_export(changeNIDMtoSPM(json),changeNIDMtoxSPM(json),changeNIDMtoTabDat(json))
+end
