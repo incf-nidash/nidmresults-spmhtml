aa6d2c828f0c86736eab2af6e60ea8f7620cf527
Tom Maullin <tommaullin@gmail.com>
2016-10-04 14:05:00 +0100

    now locates lib

--- nidm_results_display.m
+++ nidm_results_display.m
@@ -2,5 +2,9 @@ function nidm_results_display(jsonfilepath)
     json=spm_jsonread(jsonfilepath);
     [pathstr,name,ext] = fileparts(jsonfilepath); 
     json.filepath = pathstr;
+    if exist('changeNIDMtoSPM') ~= 2
+        addpath(fileparts(mfilename('fullpath')), 'lib')
+        disp(fileparts(mfilename('fullpath')), 'lib')
+    end
     spm_results_export(changeNIDMtoSPM(json),changeNIDMtoxSPM(json),changeNIDMtoTabDat(json));
 end
