ece7863bdf3cf3171a519e69a6a62c7a1fcf092f
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-11-23 12:07:13 +0000

    fix: use relative path to test data

--- test/nidmExampleDataTest.m
+++ test/nidmExampleDataTest.m
@@ -61,9 +61,10 @@ classdef nidmExampleDataTest < matlab.unittest.TestCase
         
         %Checking the nidm json is not damaged by the viewer.
         function checkNIDMUnaffected(testCase)
-            nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'Data', 'fsl_default', 'nidm.json'));
-            originalNIDM = spm_jsonread('C:\Users\owner\Documents\Project-NIDASH\nidmresults-spmhtml\Data\fsl_default\nidmWithoutMip.json');
-            currentNIDM = spm_jsonread('C:\Users\owner\Documents\Project-NIDASH\nidmresults-spmhtml\Data\fsl_default\nidm.json');
+            fsl_default_dir = fullfile(fileparts(mfilename('fullpath')), '..', 'Data', 'fsl_default');
+            nidm_results_display(fullfile(fsl_default_dir, 'nidm.json'));
+            originalNIDM = spm_jsonread(fullfile(fsl_default_dir, 'nidmWithoutMip.json'));
+            currentNIDM = spm_jsonread(fullfile(fsl_default_dir, 'nidm.json'));
             %Choose a random vertex in the graph that we know should not have been changed.
             testObject = 20;
             while testObject == 20
