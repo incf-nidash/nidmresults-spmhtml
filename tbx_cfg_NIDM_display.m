9ad79e3684904e147f0a6dfbc1d632b3e1b6a46e
Thomas Maullin-Sapey <TomM@NDPH6324.ndph.ox.ac.uk>
2018-01-22 14:18:58 +0000

    wip adding excursionset options

--- tbx_cfg_NIDM_display.m
+++ tbx_cfg_NIDM_display.m
@@ -11,6 +11,36 @@ function nidmdisplay = tbx_cfg_NIDM_display()
     addpath(toolboxDir);
     addpath(fullfile(toolboxDir, 'test'));
     
+    %Excursion sets specified numerically.
+    exSet_num          = cfg_entry;
+    exSet_num.name     = 'Excursion Set Numbers';
+    exSet_num.help    = {'',...
+       'Specify numerically which excursion sets you wish to view.',... 
+       'If unsure of the numerical values corresponding to excursion sets, choose "Select".'};
+    exSet_num.strtype = 'e';
+    exSet_num.tag      = 'exNums_num';
+    exSet_num.num     = [Inf 1];
+    
+    %Display all excursion sets.
+    exSet_all      = cfg_const;
+    exSet_all.tag  = 'allEx';
+    exSet_all.name = 'All';
+    exSet_all.val  = {0};
+    exSet_all.help = {'This will display all excursion sets in the NIDM-Pack.'};
+    
+    %Display all excursion sets.
+    exSet_select      = cfg_const;
+    exSet_select.tag  = 'selectEx';
+    exSet_select.name = 'Select';
+    exSet_select.val  = {0};
+    exSet_select.help = {'This will open the contrast selection window when the batch job is run.'};
+    
+    %Excursion set display options.
+    exSet         = cfg_choice;
+    exSet.name    = 'Excursion sets to display';
+    exSet.tag     = 'exSet';
+    exSet.values  = {exSet_num, exSet_all, exSet_select};
+    
     %Output Directory.
     outputDirCfg         = cfg_files; 
     outputDirCfg.name    = 'Output Directory'; 
@@ -30,7 +60,7 @@ function nidmdisplay = tbx_cfg_NIDM_display()
 
     %NIDM-Display 
     nidmdisplay         = cfg_exbranch;
-    nidmdisplay.val     = {nidmpack outputDirCfg} ;
+    nidmdisplay.val     = {nidmpack outputDirCfg exSet} ;
     nidmdisplay.tag     = 'NIDMdisplay';
     nidmdisplay.name    = 'NIDM-Results Display';
     nidmdisplay.help    = {'NIDM-Results Display.'}';
