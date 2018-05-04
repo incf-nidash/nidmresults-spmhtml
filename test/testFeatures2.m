e4999aaff8ec0e2a70dfaddf2db1251a2666a246
Thomas Maullin-Sapey <tommaullin@gmail.com>
2018-05-04 15:37:33 +0100

    check->test

--- test/testFeatures2.m
+++ test/testFeatures2.m
@@ -14,8 +14,8 @@ function test_suite=testFeatures2
     initTestSuite;
 end
 
-%Checking the viewer runs on SPM-nidm input.
-function checkViewerRunsSPM()
+%testing the viewer runs on SPM-nidm input.
+function testViewerRunsSPM()
     data_path = fullfile(fileparts(mfilename('fullpath')), '..', 'test', 'data', 'ex_spm_default.nidm');
     if(~exist(data_path, 'dir'))
         mkdir(data_path)
@@ -26,9 +26,9 @@ function checkViewerRunsSPM()
     nidm_results_display(data_path);
 end
 
-%Checking the experiment title is somewhere in the output HTML
+%testing the experiment title is somewhere in the output HTML
 %file.
-function checkForTitle()
+function testForTitle()
     data_path = fullfile(fileparts(mfilename('fullpath')), '..', 'test', 'data', 'ex_spm_default.nidm');
     delete_html_file(data_path);
     nidm_results_display(data_path);
@@ -36,9 +36,9 @@ function checkForTitle()
     verifySubstring(text, 'tone counting vs baseline');
 end
 
-%Checking the original functionality of the viewer with the
+%testing the original functionality of the viewer with the
 %original SPM, xSPM and TabDat functions is unaffected.
-function checkOriginalViewerRuns()
+function testOriginalViewerRuns()
     data_path = fullfile(fileparts(mfilename('fullpath')), '..', 'test', 'data', 'ex_spm_output');
     delete_html_file(data_path);
     cwd = pwd;
@@ -48,15 +48,15 @@ function checkOriginalViewerRuns()
     cd(cwd);
 end
 
-%Checking the viewer runs on FSL-nidm output.
-function checkViewerRunsFSL()
+%testing the viewer runs on FSL-nidm output.
+function testViewerRunsFSL()
     data_path = fullfile(fileparts(mfilename('fullpath')), '..', 'test', 'data', 'fsl_default_130.nidm');
     delete_html_file(data_path);
     nidm_results_display(data_path);
 end
 
-%Checking the viewer runs on SPM-nidm output with no MIP.
-function checkViewerRunsSPMwoMIP()
+%testing the viewer runs on SPM-nidm output with no MIP.
+function testViewerRunsSPMwoMIP()
     data_path = fullfile(fileparts(mfilename('fullpath')), '..', 'test', 'data', 'ex_spm_default_wo_MIP');
     %Copy contents of ex_spm_default NIDM pack.
     copyfile(fullfile(fileparts(mfilename('fullpath')), '..', 'test', 'data', 'ex_spm_default.nidm', '*'),...
@@ -70,8 +70,8 @@ function checkViewerRunsSPMwoMIP()
     nidm_results_display(data_path);
 end
         
-%         %Checking the nidm json is not damaged by the viewer.
-%         function checkNIDMUnaffected()
+%         %testing the nidm json is not damaged by the viewer.
+%         function testNIDMUnaffected()
 %             fsl_default_dir = fullfile(fileparts(mfilename('fullpath')), '..', 'test', 'data', 'fsl_default');
 %             delete_html_file(fsl_default_dir);
 %             nidm_results_display(fullfile(fsl_default_dir, 'nidm.json'));
