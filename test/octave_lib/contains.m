419ea7a20396ef50f7a76129cbc7cf3d045132ca
Thomas Maullin-Sapey <tommaullin@gmail.com>
2018-05-22 10:33:07 +0100

    wip travis tests

--- test/octave_lib/contains.m
+++
@@ -1,9 +0,0 @@
-function tf = contains(s, pattern)
-% This is a temporary fix as Octave does not yet
-% include a contains function.
-%
-% Author: Tom Maullin (04/05/18)
-
-    tf = isempty(strfind(s, pattern));
-
-end
