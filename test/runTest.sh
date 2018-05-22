9c001ef13a5c62caff943f5961daae10fd021ac3
TomMaullin <tommaullin@gmail.com>
2018-05-22 18:53:13 +0100

    outputting results

--- test/runTest.sh
+++ test/runTest.sh
@@ -15,4 +15,6 @@ cp /spmviewer/test/bug_patches/spm_file_template.m /code/spm12/
 # Run the tests. (We turn off the shadowed function warning as our copy of the
 # statistics package contains several duplicate functions which casue errors when
 # added to our path).
-octave --no-window-system --eval "warning('off','Octave:shadowed-function');addpath(genpath('/spmviewer/'));moxunit_runtests"
+testresult=$(octave --no-window-system --eval "warning('off','Octave:shadowed-function');addpath(genpath('/spmviewer/'));moxunit_runtests")
+
+echo $testresult
