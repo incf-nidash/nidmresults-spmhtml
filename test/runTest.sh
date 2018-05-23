4fd1342702c8deb384bebd3261cb6c0c5838ec63
TomMaullin <tommaullin@gmail.com>
2018-05-23 13:24:46 +0100

    Patch updates

--- test/runTest.sh
+++ test/runTest.sh
@@ -12,8 +12,7 @@ make install-octave
 cd ..
 
 # Bug fix for SPM
-rm /code/spm12/spm_file_template.m
-cp /spmviewer/test/bug_patches/spm_file_template.m /code/spm12/
+patch -p0 < /spmviewer/test/bug_patches/spm_file_template.patch /code/spm12/spm_file_template.m
 
 # Run the tests. (We turn off the shadowed function warning as our copy of the
 # statistics package contains several duplicate functions which casue errors when
