d899b4933c55b7bcdcc9005bf5d270a5e820f803
Thomas Maullin-Sapey <tommaullin@gmail.com>
2018-10-01 13:19:33 +0100

    mox test update

--- test/moxunit_testsuite/testDataSets.m
+++ test/moxunit_testsuite/testDataSets.m
@@ -128,93 +128,100 @@ function test_ex_spm_thr_voxelunct4()
     nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','ex_spm_thr_voxelunct4.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_con_f_130.nidm
-function test_fsl_con_f_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_con_f_130.nidm.zip');
+%Test viewer displays fsl_con_f.nidm
+function test_fsl_con_f()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_con_f.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_con_f_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_con_f.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_contrast_mask_130.nidm
-function test_fsl_contrast_mask_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_contrast_mask_130.nidm.zip');
+%Test viewer displays fsl_con_f.nidm
+function test_fsl_con_f_multiple()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_con_f_multiple.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_contrast_mask_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_con_f_multiple.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_default_130.nidm
-function test_fsl_default_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_default_130.nidm.zip');
+%Test viewer displays fsl_contrast_mask.nidm
+function test_fsl_contrast_mask()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_contrast_mask.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_default_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_contrast_mask.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_full_examples001_130.nidm
-function test_fsl_full_examples001_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_full_examples001_130.nidm.zip');
+%Test viewer displays fsl_default.nidm
+function test_fsl_default()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_default.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_full_examples001_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_default.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_gamma_basis_130.nidm
-function test_fsl_gamma_basis_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_gamma_basis_130.nidm.zip');
+%Test viewer displays fsl_full_examples001.nidm
+function test_fsl_full_examples001()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_full_examples001.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_gamma_basis_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_full_examples001.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_gaussian_130.nidm
-function test_fsl_gaussian_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_gaussian_130.nidm.zip');
+%Test viewer displays fsl_gamma_basis.nidm
+function test_fsl_gamma_basis()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_gamma_basis.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_gaussian_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_gamma_basis.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_group_btw_130.nidm
-function test_fsl_group_btw_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_group_btw_130.nidm.zip');
+%Test viewer displays fsl_gaussian.nidm
+function test_fsl_gaussian()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_gaussian.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_group_btw_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_gaussian.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_group_ols_130.nidm
-function test_fsl_group_ols_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_group_ols_130.nidm.zip');
+%Test viewer displays fsl_group_btw.nidm
+function test_fsl_group_btw()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_group_btw.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_group_ols_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_group_btw.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_group_wls_130.nidm
-function test_fsl_group_wls_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_group_wls_130.nidm.zip');
+%Test viewer displays fsl_group_ols.nidm
+function test_fsl_group_ols()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_group_ols.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_group_wls_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_group_ols.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_hrf_fir_130.nidm
-function test_fsl_hrf_fir_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_hrf_fir_130.nidm.zip');
+%Test viewer displays fsl_group_wls.nidm
+function test_fsl_group_wls()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_group_wls.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_hrf_fir_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_group_wls.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_hrf_gammadiff_130.nidm
-function test_fsl_hrf_gammadiff_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_hrf_gammadiff_130.nidm.zip');
+%Test viewer displays fsl_hrf_fir.nidm
+function test_fsl_hrf_fir()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_hrf_fir.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_hrf_gammadiff_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_hrf_fir.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_thr_clustfwep05_130.nidm
-function test_fsl_thr_clustfwep05_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_thr_clustfwep05_130.nidm.zip');
+%Test viewer displays fsl_hrf_gammadiff.nidm
+function test_fsl_hrf_gammadiff()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_hrf_gammadiff.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_thr_clustfwep05_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_hrf_gammadiff.nidm.zip'), 'All');
 end
 
-%Test viewer displays fsl_thr_voxelfwep05_130.nidm
-function test_fsl_thr_voxelfwep05_130()
-    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_thr_voxelfwep05_130.nidm.zip');
+%Test viewer displays fsl_thr_clustfwep05.nidm
+function test_fsl_thr_clustfwep05()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_thr_clustfwep05.nidm.zip');
     delete_html_file(strrep(data_path, '.zip', ''));
-    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_thr_voxelfwep05_130.nidm.zip'), 'All');
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_thr_clustfwep05.nidm.zip'), 'All');
+end
+
+%Test viewer displays fsl_thr_voxelfwep05.nidm
+function test_fsl_thr_voxelfwep05()
+    data_path = fullfile(fileparts(mfilename('fullpath')),'..','data','fsl_thr_voxelfwep05.nidm.zip');
+    delete_html_file(strrep(data_path, '.zip', ''));
+    nidm_results_display(fullfile(fileparts(mfilename('fullpath')), '..', 'data','fsl_thr_voxelfwep05.nidm.zip'), 'All');
 end
